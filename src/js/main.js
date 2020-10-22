771ee9fd179c5387d99924356184615c7e01ed84
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-22 18:24:34 +0200

    Create function to call API and get data

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,13 @@
 const searchingGap = document.querySelector(".js-search");
 const button = document.querySelector(".js-btn");
 
+const triggerSearch = () => {
+  let searchingGapValue = searchingGap.value;
+  fetch(`http://api.tvmaze.com/search/shows?q=${searchingGapValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data[0].score);
+    });
+};
+
 button.addEventListener("click", triggerSearch);
