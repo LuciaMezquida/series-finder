8e6be90f63fd5de8d3173bb5078a67ce3bbe9e00
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-23 14:06:17 +0200

    Add function to add favourites

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ const searchingGap = document.querySelector(".js-search");
 const button = document.querySelector(".js-btn");
 const resultsList = document.querySelector(".js-results-list");
 const favouriteList = document.querySelector(".js-favourite-list");
+
+//Call API to get info
 let dataList = [];
 const triggerSearch = () => {
   let searchingGapValue = searchingGap.value;
@@ -12,23 +14,54 @@ const triggerSearch = () => {
     .then((data) => {
       dataList = data;
       paintShows();
+      if (dataList.length == 0) {
+        resultsList.innerHTML = "<p class ='not-found-message'>No se han encontrado resultados</p>";
+      }
+      listenListResults();
     });
 };
+//Paint data into the browser
 const paintShows = () => {
-  let html = "";
+  let htmlResult = "";
   for (let i = 0; i < dataList.length; i++) {
-    html += "<li>";
-    html += "<div>";
-    html += `<h3 class="list-title">${dataList[i].show.name}</h3>`;
+    htmlResult += `<li id="${i}" class="js-list">`;
+    htmlResult += "<div>";
+    htmlResult += `<h3 class="list-title">${dataList[i].show.name}</h3>`;
     if (dataList[i].show.image === null) {
-      html += `<img src="https://srv.latostadora.com/designall.dll/cat_not_available--i:14138557810514138520;x:20;w:520;m:1.jpg" width="210" height="250" alt="Image not found"/>`;
+      htmlResult += `<img src="https://srv.latostadora.com/designall.dll/cat_not_available--i:14138557810514138520;x:20;w:520;m:1.jpg" width="210" height="260" alt="Image not found"/>`;
     } else {
-      html += `<img class="list-image" src="${dataList[i].show.image.medium}" alt="image of ${dataList[i].show.name}"/>`;
+      htmlResult += `<img class="list-image" src="${dataList[i].show.image.medium}" alt="Image of ${dataList[i].show.name}"/>`;
     }
-    html += "</div>";
-    html += "</li>";
+    htmlResult += "</div>";
+    htmlResult += "</li>";
   }
-  resultsList.innerHTML = html;
+  resultsList.innerHTML = htmlResult;
+};
+
+//Paint favourites list
+let favouritesDataList = [];
+const keepFavourites = (event) => {
+  let selectedListId = event.currentTarget.id;
+  const indexFav = favouritesDataList.indexOf(dataList[selectedListId]);
+  console.log(indexFav);
+  if (indexFav === -1) {
+    favouritesDataList.push(dataList[selectedListId]);
+  }
+  // else {
+  //   favouritesDataList.splice(indexFav, 1);
+  // }
+
+  //favouriteList.innerHTML = htmlFavourite;
+  console.log(favouritesDataList);
+  //console.log(event.currentTarget);
+  //console.log(htmlFavourite);
+};
+//listen every list item
+const listenListResults = () => {
+  const listResults = document.querySelectorAll(".js-list");
+  for (let item of listResults) {
+    item.addEventListener("click", keepFavourites);
+  }
 };
 
 button.addEventListener("click", triggerSearch);
